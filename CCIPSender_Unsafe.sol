11c8ab9fe004d0df88863489c533167def88380b
0xgeorgemathew <georgekurianmathew@gmail.com>
2023-11-12 06:21:23 +0530

    dgit

---
+++ CCIPSender_Unsafe.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import {Client} from "@chainlink/contracts-ccip/src/v0.8/ccip/libraries/Client.sol";
+import {IRouterClient} from "@chainlink/contracts-ccip/src/v0.8/ccip/interfaces/IRouterClient.sol";
+import {LinkTokenInterface} from "@chainlink/contracts/src/v0.8/interfaces/LinkTokenInterface.sol";
+
+contract CCIPSender_Unsafe {
+    address link;
+    address router;
+
+    constructor (address _link, address _router){
+        link = _link;
+        router = _router;
+        LinkTokenInterface(link).approve(router, type(uint256).max);
+    }
+    function send(address _receiver,) public {
+        Client.EVM2AnyMessage memory message =  Client.EVM2AnyMessage({
+            receiver: abi.encode(_receiver);
+        });
+    }
+}
